88be9cac2a9a4779e4f0a6c7a2414515b6127a34
Rick Belson <belsonrj@aol.com>
2020-03-26 00:23:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -70,9 +70,25 @@ end
 
   def self.students_below_12th_grade
     sql = <<-SQL
-       SELECT COUNT(grade > 10) FROM students
+    SELECT * 
+    FROM students 
+    WHERE students.grade < 12
     SQL
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+    SELECT * 
+    FROM students
+    WHERE students.grade = 10
+    LIMIT 1
+    SQL
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def self.first_X_students_in_grade_10(x)
